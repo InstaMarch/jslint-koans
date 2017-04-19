355dbf7495c64c57f7c3a4b2dbffdfb8b1332887
Ben Pardo <benpardo@Bens-MBP-4.lan>
2017-04-19 17:46:21 -0600

    adding a with operator koan

---
+++ koans/3.js
@@ -0,0 +1,14 @@
+//You may transcend beyond the grasp of an idol of machinery
+
+var russianDoll = {
+    playWithinAPlay: {
+        recursion: {
+            sharingGoogleHangoutsScreen: {}
+        }
+    }
+};
+
+with (russianDoll.playWithinAPlay.recursion.sharingGoogleHangoutsScreen) {
+    parallelMirrors = true;
+    thePyramidSchemeToEndAllPyramidSchemes = true;
+}
