77357db9ff08c4995c6323e3f01e07b35e2a1388
Ben Pardo <benpardo@Bens-MBP-4.lan>
2017-04-19 22:38:02 -0600

    adding fat arrow body koan

---
+++ koans/8.js
@@ -0,0 +1,11 @@
+/*jslint
+    es6
+*/
+//Some functions will plant tangling seeds
+
+let addMore = (firstNumber, secondNumber) => {
+    let total = 0;
+    total += firstNumber;
+    total += secondNumber;
+    return total;
+};
